a11228996cfe5aecab04b1b33bbc03246acb14bb
mfcc168 <88373848+mfcc168@users.noreply.github.com>
2025-03-04 11:12:49 +0800

    update dotenv

--- src/employee/babel.config.js
+++ src/employee/babel.config.js
@@ -1,18 +1,33 @@
-module.exports = function(api) {
+module.exports = function (api) {
     api.cache(true);
-
+  
     return {
-        presets: [["babel-preset-expo", {
-            jsxImportSource: "nativewind"
-        }], "nativewind/babel"],
-
-        plugins: [["module-resolver", {
+      presets: [
+        [
+          "babel-preset-expo",
+          {
+            jsxImportSource: "nativewind", // Add Nativewind JSX support here
+          },
+        ],
+      ],
+      plugins: [
+        [
+          "module-resolver",
+          {
             root: ["./"],
-
             alias: {
-                "@": "./",
-                "tailwind.config": "./tailwind.config.js"
-            }
-        }]]
+              "@": "./",
+              "tailwind.config": "./tailwind.config.js",
+            },
+          },
+        ],
+        [
+          "module:react-native-dotenv",
+          {
+            moduleName: "@env",
+            path: ".env",
+          },
+        ],
+      ],
     };
-};
+  };
