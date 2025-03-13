9eab9181bdc67294ce943366175d2571d1daf970
mfcc168 <88373848+mfcc168@users.noreply.github.com>
2025-03-13 14:46:49 +0800

    Update firebaseConfig.ts

--- src/employee/app/firebaseConfig.ts
+++ src/employee/app/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTce8kSu7LlDcgF3SJzUGt_KcQWEEkotQ",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_APIKEY,
   authDomain: "lafarge-management.firebaseapp.com",
   projectId: "lafarge-management",
   storageBucket: "lafarge-management.firebasestorage.app",
   messagingSenderId: "65778745058",
-  appId: "1:65778745058:android:5f11a6dcd1d19ffa08a8bb",
+  appId: process.env.EXPO_PUBLIC_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
